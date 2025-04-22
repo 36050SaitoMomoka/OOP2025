c1adf5659a5adaaaa3401abcf69bb2f79cc46897
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-04-22 11:02:44 +0900

    静的クラスに変更

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,8 +1,9 @@
-﻿namespace DistanceConverter {
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DistanceConverter {
     internal class Program {
         //コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
-
             int start = int.Parse(args[1]);
             int end = int.Parse(args[2]);
 
@@ -15,19 +16,21 @@
 
                 //フィートからメートルへの対応表を出力
                 static void PrintFeetToMeterList(int start, int end) {
-                    FeetConverter converter = new FeetConverter();
+                    //FeetConverter converter = new FeetConverter();
                     for (int feet = start; feet <= end; feet++) {
-                        double meter = converter.ToMeter(feet);
+                        //double meter = converter.ToMeter(feet);
+                        double meter = FeetConverter.ToMeter(feet);
                         Console.WriteLine($"{feet}ft = {meter:0.0000}m");
                     }
                 }
 
                 //メートルからフィートへの対応表を出力
                 static void PrintMeterToFeetList(int start, int end) {
-                    FeetConverter converter = new FeetConverter();
+                    //FeetConverter converter = new FeetConverter();
                     for (int meter = start; meter <= end; meter++) {
                         //double feet = meter * 0.3048;
-                        double feet = converter.FromMeter(meter);
+                        //double feet = converter.FromMeter(meter);
+                        double feet = FeetConverter.FromMeter(meter);
                         Console.WriteLine($"{meter}m = {feet:0.0000}ft");
                     }
                 }
