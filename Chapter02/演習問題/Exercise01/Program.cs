ee5b1d74a5744a963bd30817b59bc02094399a4d
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-05-13 11:26:59 +0900

    2.1.4完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Exercise01 {
     public class Program {
         static void Main(string[] args) {
-            //2.1.3
+            //2.1.3 【インスタンス複数作成・配列songsに格納】
             var songs = new Song[] {
             new Song("Let it be", "The Beatles", 243),
             new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
@@ -9,10 +9,17 @@
             new Song("Honesty", "Billy Joel", 231),
             new Song("I Will Always Love You", "Whitney Houston", 273),
             };
+
+            printSongs(songs);
         }
 
         //2.1.4
-        private static void printSongs(Song[] songs) {
+        public static void printSongs(Song[] songs) {
+            foreach (var song in songs) {
+                var minutes = song.Length / 60;
+                var seconds = song.Length % 60;
+                Console.WriteLine($"{song.Title},{song.ArtistName},{minutes}:{seconds:00}");
+            }
         }
     }
 }
