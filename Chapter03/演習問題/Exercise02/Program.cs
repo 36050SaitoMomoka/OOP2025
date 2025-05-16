5f7ee79c6211ca6f9b88549b8309f6c3986011e4
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-05-16 15:36:54 +0900

    3.2.1完成

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            Console.WriteLine("都市名を入力。空行で終了");
+            do {
+                var name = Console.ReadLine();
+                int index = cities.FindIndex(s => s == name);
+                if (string.IsNullOrEmpty(name)) {
+                    index = -1;
+                    break;
+                }
+                Console.WriteLine(index);
+            } while (true);
+        }
+
+        private static void Exercise2_2(List<string> cities) {
+
+        }
+
+        private static void Exercise2_3(List<string> cities) {
+
+        }
+
+        private static void Exercise2_4(List<string> cities) {
+
+        }
+    }
+}
