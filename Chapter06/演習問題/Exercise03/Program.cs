4fc944cdff4e3dd112f22af263eebd2d59adca40
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-06-04 10:32:02 +0900

    6.3.99完成

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -6,20 +6,24 @@ namespace Exercise03 {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
             #region
-            Console.WriteLine("6.3.1");
-            Exercise1(text);
+            //Console.WriteLine("6.3.1");
+            //Exercise1(text);
 
-            Console.WriteLine("6.3.2");
-            Exercise2(text);
+            //Console.WriteLine("6.3.2");
+            //Exercise2(text);
 
-            Console.WriteLine("6.3.3");
-            Exercise3(text);
+            //Console.WriteLine("6.3.3");
+            //Exercise3(text);
 
-            Console.WriteLine("6.3.4");
-            Exercise4(text);
+            //Console.WriteLine("6.3.4");
+            //Exercise4(text);
 
-            Console.WriteLine("6.3.5");
-            Exercise5(text);
+            //Console.WriteLine("6.3.5");
+            //Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+
             #endregion
         }
 
@@ -36,11 +40,18 @@ namespace Exercise03 {
             var sb = new StringBuilder();
             foreach (var word in array) {
                 sb.Append(word + " ");
-            }            
+            }
             var words = sb.ToString().TrimEnd();
             Console.WriteLine(words + ".");
-        }
 
+            //解答
+            //var array = text.Split(' ');
+            //var sb = new StringBuilder(array[0]);
+            //foreach (var word in array.Skip(1)) {
+            //    sb.Append(" ");
+            //    sb.Append(word);
+            //}
+        }
 
         private static void Exercise4(string text) {
             var words = text.Split([' '], StringSplitOptions.RemoveEmptyEntries);
@@ -56,6 +67,22 @@ namespace Exercise03 {
                 .Where(x => x.Length <= 4)
                 .ToList()
                 .ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise6(string text) {
+            var str = text.Replace(" ", "").ToLower();
+            var cr = str.OrderBy(c => c).ToArray();
+            //Console.WriteLine(cr);    //並べ替えた文字列を出力する
+            int i = 0;
+            while (i < cr.Length) {
+                int count = 1;
+                while (i < cr.Length - 1 && cr[i] == cr[i + 1]) {
+                    count++;
+                    i++;
+                }
+                Console.WriteLine($"{cr[i]}:{count}");
+                i++;
+            }
         }
     }
 }
