c2f13d59ef9c25cd3aaf129f720d74fef61748f9
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-06-06 14:51:29 +0900

    5.1.1完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get;}
+        public int Month { get; }
+
+        public YearMonth(int Year, int Month) {
+            Year = Year;
+            Month = Month;
+        }
+    }
+}
--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Data.SqlTypes;
 using System.Text;
 
 namespace Exercise03 {
@@ -6,20 +7,20 @@ namespace Exercise03 {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
             #region
-            //Console.WriteLine("6.3.1");
-            //Exercise1(text);
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
 
-            //Console.WriteLine("6.3.2");
-            //Exercise2(text);
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
 
-            //Console.WriteLine("6.3.3");
-            //Exercise3(text);
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
 
-            //Console.WriteLine("6.3.4");
-            //Exercise4(text);
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
 
-            //Console.WriteLine("6.3.5");
-            //Exercise5(text);
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
 
             Console.WriteLine("6.3.99");
             Exercise6(text);
@@ -82,6 +83,35 @@ namespace Exercise03 {
                 }
                 Console.WriteLine($"{cr[i]}:{count}");
                 i++;
+            }
+
+            //解答（アルファベットの数をカウントして表示する）
+            var st = text.ToLower().Replace(" ", "");
+            var alphDicCount = Enumerable.Range('a', 26)
+                            .ToDictionary(num => ((char)num).ToString(), num => 0);
+            foreach (var alph in st) {
+                alphDicCount[alph.ToString()]++;
+            }
+            foreach (var item in alphDicCount) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            Console.WriteLine();    //改行
+
+            //解答（配列で集計)
+            var array = Enumerable.Repeat(0, 26).ToArray();
+            foreach (var alph in st) {
+                array[alph - 'a']++;
+            }
+            for (char ch = 'a'; ch < 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
+
+            Console.WriteLine();    //改行
+
+            //解答（'a'から順にカウントして出力）
+            for (char ch = 'a'; ch < 'z'; ch++) {
+                Console.WriteLine($"{ch}:{ text.Count(tc => tc == ch) }");
             }
         }
     }
