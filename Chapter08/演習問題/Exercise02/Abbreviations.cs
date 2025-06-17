950264c213523baf95445eabac0608aec8dca558
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-06-17 10:16:56 +0900

    8.2.1完成 8.2.2完成

--- Chapter08/演習問題/Exercise02/Abbreviations.cs
+++ Chapter08/演習問題/Exercise02/Abbreviations.cs
@@ -31,4 +31,11 @@ public class Abbreviations {
             }
         }
     }
+    //8.2.1
+    public int Count => _dict.Count();
+
+    //8.2.2
+    public bool Remove(string abb) => _dict.Remove(abb);
+
+
 }
