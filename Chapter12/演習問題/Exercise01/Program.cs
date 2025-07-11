26d7484ae4fa754ea928db91abf2d32034912b69
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-07-11 15:41:37 +0900

    12.1.2完成 12.1.1修正

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Unicode;
 using System.Text.Encodings.Web;
+using System.Text;
 
 namespace Exercise01 {
     internal class Program {
@@ -14,20 +15,49 @@ namespace Exercise01 {
             Console.WriteLine(jsonString);
             var obj = Deserialize(jsonString);
             Console.WriteLine(obj);
+
+            //問題12.1.2
+            Employee[] employees = [
+                new () {
+                    Id = 123,
+                    Name = "山田太郎",
+                    HireDate = new DateTime(2018, 10, 1),
+                },
+                new () {
+                    Id = 198,
+                    Name = "田中華子",
+                    HireDate = new DateTime(2020, 4, 1),
+                },
+            ];
+            Serialize("employees.json", employees);
         }
 
+        //問題12.1.1
         static string Serialize(Employee emp) {
             var options = new JsonSerializerOptions {
                 WriteIndented = true,
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             };
             string jsonString = JsonSerializer.Serialize(emp, options);
             return jsonString;
         }
 
+        //問題12.1.1
         static Employee? Deserialize(string jsonString) {
             var employee = JsonSerializer.Deserialize<Employee>(jsonString);
             return employee;
+        }
+
+        //問題12.1.2
+        //シリアル化してファイルへ出力する
+        static void Serialize(string filePath, IEnumerable<Employee> employees) {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            string jsonString = JsonSerializer.Serialize(employees, options);
+            File.WriteAllText(filePath, jsonString);
         }
 
         public record Employee {
