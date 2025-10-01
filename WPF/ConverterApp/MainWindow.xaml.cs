0ec9e2169d1b352659a896e7fb77f9b6d3ebda6f
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-10-01 12:11:00 +0900

    コンボボックス完成

--- WPF/ConverterApp/MainWindow.xaml.cs
+++ WPF/ConverterApp/MainWindow.xaml.cs
@@ -13,16 +13,44 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ConverterApp
-{
+namespace ConverterApp {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+
+            MetricUnit.Items.Add("mm");
+            MetricUnit.Items.Add("cm");
+            MetricUnit.Items.Add("m");
+            MetricUnit.Items.Add("km");
+
+            ImperialUnit.Items.Add("in");
+            ImperialUnit.Items.Add("ft");
+            ImperialUnit.Items.Add("yd");
+            ImperialUnit.Items.Add("mi");
+        }
+
+        Dictionary<string, double> metric = new Dictionary<string, double> {
+            {"mm",1 },
+            {"cm",10 },
+            {"m",100 },
+            {"km",1000 },
+        };
+
+        Dictionary<string, double> imperial = new Dictionary<string, double> {
+            {"in",25.4 },
+            {"ft",304.8 },
+            {"yd",914.4 },
+            {"mi",1609344 },
+        };
+
+        private void MetricToImperialUnit_Click(object sender, RoutedEventArgs e) {
+            int selectedMetricUnit = MetricUnit.SelectedIndex;
+            int selectedImperialUnit = ImperialUnit.SelectedIndex;
+
+
         }
     }
 }
