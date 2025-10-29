a1acb5d8ea7efd646b1afd45150f1cdffbeb6622
SaitoMomoka <oibc36050@ojs.ac.jp>
2025-10-29 09:54:17 +0900

    13.1.3完成

--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -2,18 +2,25 @@
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine("(2)");
             Exercise1_2();
             Console.WriteLine();
+            Console.WriteLine("(3)");
             Exercise1_3();
             Console.WriteLine();
+            Console.WriteLine("(4)");
             Exercise1_4();
             Console.WriteLine();
+            Console.WriteLine("(5)");
             Exercise1_5();
             Console.WriteLine();
+            Console.WriteLine("(6)");
             Exercise1_6();
             Console.WriteLine();
+            Console.WriteLine("(7)");
             Exercise1_7();
             Console.WriteLine();
+            Console.WriteLine("(8)");
             Exercise1_8();
 
             Console.ReadLine();
@@ -25,7 +32,17 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_3() {
+            var groups = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(x => new {
+                    PublisheYear = x.Key,
+                    Count = x.Count()
+                })
+                .OrderBy(b => b.PublisheYear);
 
+            foreach (var item in groups) {
+                Console.WriteLine($"{item.PublisheYear}年：{item.Count}");
+            }
         }
 
         private static void Exercise1_4() {
